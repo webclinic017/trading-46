cd1497ee54236c3457621a2c2ee2a784dbf09637
oscar0608@dragonflyuas.com.tw <oscar0608@dragonflyuas.com.tw>
2023-03-27 19:01:51 +0800

    add backtesting finmind tool

---
+++ finmindpractice.py
@@ -0,0 +1,21 @@
+# 取得股價
+from FinMind.data import DataLoader
+
+dl = DataLoader()
+# 下載台股股價資料
+stock_data = dl.taiwan_stock_daily(
+    stock_id='2609', start_date='2018-01-01', end_date='2021-06-26'
+)
+# 下載三大法人資料
+stock_data = dl.feature.add_kline_institutional_investors(
+    stock_data
+) 
+# 下載融資券資料
+stock_data = dl.feature.add_kline_margin_purchase_short_sale(
+    stock_data
+)
+
+# 繪製k線圖
+from FinMind import plotting
+
+plotting.kline(stock_data)
